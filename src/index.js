10a0f64a8a407129a39a5aa8248624fa48ba838c
Vitalii Khivrych <khivr.vitali@gmail.com>
2022-12-01 16:34:24 +0200

    f-01: add theme, globalStyle, fonts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
+import 'modern-normalize';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Global, ThemeProvider } from '@emotion/react';
+
+import { GlobalStyles, theme } from 'styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyles} />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
