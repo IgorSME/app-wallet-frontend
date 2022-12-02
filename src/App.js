0d305c530f473525499a6e24960a7e0f45440002
AnnaNychyk <annanichichka@gmail.com>
2022-12-02 16:47:09 +0200

    navigation without styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Suspense } from 'react';
+
+import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
+import PublicRoute from 'components/PublicRoute/PublicRoute';
+import Navigation from 'components/Navigation/Navigation';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+      <Suspense fallBack={<p>...loading</p>}>
+        <Routes>
+          <Route element={<PublicRoute />}>
+            <Route path="/register" element={<h1>Register page</h1>} />
+            <Route path="/login" element={<h1>Login page</h1>} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/home" element={<h1>Home page</h1>} />
+            <Route path="/statistics" element={<h1>Statistics page</h1>} />
+            <Route path="/currency" element={<h1>Currency page</h1>} />
+          </Route>
+          <Route path="*" element={<p>Not Found page</p>} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,13 +1,6 @@
-// import { Navigate, Outlet } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 
-// import useAuth from 'shared/hooks/useAuth';
-
 const PrivateRoute = () => {
-  // const isLogin = useAuth();
-  // if (!isLogin) {
-  //   return <Navigate to="/login" />;
-  // }
   return <Outlet />;
 };
 export default PrivateRoute;
