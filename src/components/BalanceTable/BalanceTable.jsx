f9003dd7b8f7e07b742e230c171fba3749ec7b77
AnnaNychyk <annanichichka@gmail.com>
2022-12-06 15:45:29 +0200

    do BalanceTable

--- src/components/BalanceTable/BalanceTable.jsx
+++ src/components/BalanceTable/BalanceTable.jsx
@@ -1,8 +1,59 @@
+import {
+  Section,
+  Container,
+  Table,
+  Thead,
+  TheadTr,
+  TheadTrTh,
+  Tbody,
+  TbodyTr,
+} from 'components/BalanceTable/BalanceTable.styled';
+
 const BalanceTable = () => {
   return (
-    <>
-      <p>Balance table</p>
-    </>
+    <Section>
+      <Container>
+        <Table>
+          <Thead>
+            <TheadTr>
+              <TheadTrTh>Date</TheadTrTh>
+              <th>Type</th>
+              <th>Category</th>
+              <th>Comment</th>
+              <th>Sum</th>
+              <TheadTrTh>Balance</TheadTrTh>
+            </TheadTr>
+          </Thead>
+
+          <Tbody>
+            <TbodyTr>
+              <td>04.01.22</td>
+              <td> - </td>
+              <td>Other</td>
+              <td>Gift for your wife</td>
+              <td>300.00</td>
+              <td>6 900.00</td>
+            </TbodyTr>
+            <TbodyTr>
+              <td>05.01.19</td>
+              <td> + </td>
+              <td>Regular Income</td>
+              <td>January bonus</td>
+              <td>8 000.00</td>
+              <td>14 900.00</td>
+            </TbodyTr>
+            <TbodyTr>
+              <td>07.01.19</td>
+              <td> - </td>
+              <td>Car</td>
+              <td>Oil</td>
+              <td>1 000.00</td>
+              <td>13 900.00</td>
+            </TbodyTr>
+          </Tbody>
+        </Table>
+      </Container>
+    </Section>
   );
 };
 
--- src/components/BalanceTable/BalanceTable.styled.js
+++ src/components/BalanceTable/BalanceTable.styled.js
@@ -2,34 +2,102 @@ import styled from '@emotion/styled';
 import { ContainerStyled } from 'components/common/commonComponents.styled';
 
 export const Section = styled.section`
-  margin: 0 20px;
-  margin-bottom: 32px;
-  background-color: #fff;
-  /* backdrop-filter: blur(25px); */
+  margin: 0 32px;
   @media screen and (min-width: 768px) {
     margin: 0 32px;
-    margin-top: 28px;
-    margin-bottom: 20px;
-    margin-right: 32px;
   }
 `;
 
 export const Container = styled(ContainerStyled)`
-  padding-left: 32px;
-  padding-top: 8px;
-  padding-bottom: 12px;
-  background-color: #e7eaf2;
-  border-radius: 30px;
-  text-align: left;
+  width: 704px;
+  @media screen and (min-width: 768px) {
+    width: 704px;
+  }
+`;
+
+export const Table = styled.table`
   @media screen and (min-width: 320px) {
-    width: 280px;
   }
   @media screen and (min-width: 768px) {
-    margin: 0;
-    padding-left: 40px;
-    width: 336px;
+    width: 704px;
+    overflow: hidden;
+    border-top-left-radius: 30px;
+    border-top-right-radius: 30px;
+    border-collapse: collapse;
   }
   @media screen and (min-width: 1280px) {
-    width: 395px;
+  }
+`;
+
+export const Thead = styled.thead`
+  @media screen and (min-width: 320px) {
+  }
+  @media screen and (min-width: 768px) {
+    height: 58px;
+    background-color: #e7eaf2;
+  }
+  @media screen and (min-width: 1280px) {
+  }
+`;
+
+export const TheadTr = styled.tr`
+  @media screen and (min-width: 320px) {
+  }
+  @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 1280px) {
+  }
+`;
+
+export const TheadTrTh = styled.th`
+  @media screen and (min-width: 320px) {
+  }
+  @media screen and (min-width: 768px) {
+    &:first-child {
+      border-bottom-left-radius: 30px;
+    }
+    &:last-child {
+      border-bottom-right-radius: 30px;
+    }
+  }
+  @media screen and (min-width: 1280px) {
+  }
+`;
+
+export const Tbody = styled.tbody`
+  text-align: center;
+  font-family: 'Circe';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 1.5;
+  color: #000000;
+  background-color: transparent;
+  @media screen and (min-width: 320px) {
+  }
+  @media screen and (min-width: 768px) {
+    text-align: center;
+    font-family: 'Circe';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 1.5;
+    color: #000000;
+    background-color: transparent;
+  }
+  @media screen and (min-width: 1280px) {
+  }
+`;
+
+export const TbodyTr = styled.tr`
+  @media screen and (min-width: 320px) {
+  }
+  @media screen and (min-width: 768px) {
+    &:not(:last-child) {
+      border-bottom: 1px solid #dcdcdf;
+      box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.6);
+    }
+  }
+  @media screen and (min-width: 1280px) {
   }
 `;
