b513d9556c1007141e94ca6ecb8d340afa73ee21
Vitalii Khivrych <khivr.vitali@gmail.com>
2022-12-10 23:36:00 +0200

    refactoring register page

--- src/components/RegisterForm/RegisterForm.styled.js
+++ src/components/RegisterForm/RegisterForm.styled.js
@@ -9,27 +9,38 @@ import RegisterDecorationImgTablet from 'images/register/Register_tablet_decorat
 import RegisterDecorationImgDesktop from 'images/register/Register_desktop_decoration_img.png';
 
 export const RegisterFormStyled = styled(LoginFormStyled)`
-  @media screen and (min-width: 1280px) {
+  @media (min-width: 768px) {
     height: 616px;
-    top: 52px;
   }
 `;
 
-export const RegisterContainerStyled = styled(LoginContainerStyled)`
-  @media screen and (min-width: 768px) {
-    padding-bottom: 48px;
-    background-image: none;
-    background-image: url(${RegisterDecorationImgTablet});
-  }
-  @media screen and (min-width: 1280px) {
-    padding-bottom: 0;
-    background-image: none;
-  }
-`;
+// export const RegisterContainerStyled = styled(LoginContainerStyled)`
+//   @media screen and (min-width: 768px) {
+//     padding-bottom: 48px;
+//     background-image: none;
+//     background-image: url(${RegisterDecorationImgTablet});
+//   }
+//   @media screen and (min-width: 1280px) {
+//     padding-bottom: 0;
+//     background-image: none;
+//   }
+// `;
 
 export const RegisterSideBareStyled = styled(LoginSideBareStyled)`
+  @media screen and (min-width: 768px) {
+    width: 274px;
+    height: 250px;
+
+    background-image: url(${RegisterDecorationImgTablet});
+  }
+
   @media screen and (min-width: 1280px) {
-    background-image: none;
+    width: 452px;
+    height: 413px;
+
+    margin-right: 31;
+    margin-bottom: 32px;
+
     background-image: url(${RegisterDecorationImgDesktop});
   }
 `;
