8d84c3963f5321a3115929f28c31ff3b6a9fa730
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-12-13 01:02:35 +0100

    Update ModalAddTransaction.jsx

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react';
 import { useId } from 'react';
-import { useSelector } from 'react-redux';
-
-import get from 'redux/categories/categories-operations';
 
 import Calendar from '../Calendar/Calendar';
 import Switch from 'components/Switch/Switch';
@@ -24,11 +21,14 @@ import {
   Amound,
   Comments,
   ModalButtonStyled,
-  CalendarImg
+  CalendarImg,
 } from './ModalAddTransaction.styled';
 
-
-export default function ModalAddTransaction({ onClose: handleClose, onSubmit, categories: {baseCategories} } ) {
+export default function ModalAddTransaction({
+  onClose: handleClose,
+  onSubmit,
+  categories: { baseCategories },
+}) {
   const [isToggled, setIsToggled] = useState(false);
   const [isActive, setIsActive] = useState(false);
   const [selected, setSelected] = useState('Select a category');
@@ -37,7 +37,6 @@ export default function ModalAddTransaction({ onClose: handleClose, onSubmit, ca
   const [comment, setComment] = useState('');
 
   const inputId = useId();
- 
 
   const handleChange = event => {
     const { name, value } = event.target;
@@ -52,17 +51,16 @@ export default function ModalAddTransaction({ onClose: handleClose, onSubmit, ca
         setAmound(value);
         break;
       case 'date':
-          setDate(value);
+        setDate(value);
         break;
-        case 'comment':
-          setComment(value);
-          break;
+      case 'comment':
+        setComment(value);
+        break;
 
       default:
         break;
-        }
-    };
-
+    }
+  };
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -73,66 +71,72 @@ export default function ModalAddTransaction({ onClose: handleClose, onSubmit, ca
     setComment('');
     setIsToggled(false);
     console.log(onSubmit());
-};
+  };
 
   return (
     <>
       <ModalBackdrop>
         <ModalContainerStyled onSubmit={handleSubmit}>
-                        <ModalCloseBtn onClick={handleClose}>
-                <CloseBtnIcon/>
-            </ModalCloseBtn>
-            <ModalTitle>Add transaction</ModalTitle>
+          <ModalCloseBtn onClick={handleClose}>
+            <CloseBtnIcon />
+          </ModalCloseBtn>
+          <ModalTitle>Add transaction</ModalTitle>
           <Switch
             value={isToggled}
             id={inputId}
             isToggled={isToggled}
-            onToggle={() => setIsToggled(!isToggled)} />
-           {/* {isToggled && (  */}
-          <SelectWrapper>     
+            onToggle={() => setIsToggled(!isToggled)}
+          />
+          {/* {isToggled && (  */}
+          <SelectWrapper>
             <SelectCategoryButton
               readOnly
-              onClick={(e) => setIsActive(!isActive)}
+              onClick={e => setIsActive(!isActive)}
               value={selected}
               name="selected"
-              onChange={handleChange}/>
-                <SelectIconSvg/>
-                {isActive && (
-                    <SelectCategoryList >
-                        {baseCategories.map(({_id, categoryName, type}) => (
-                          <SelectCategoryItem
-                            key={_id}
-                            name={categoryName}
-                            type={type}
-                                onClick={(e) => {
-                                setSelected(categoryName);
-                                setIsActive(false);
-                            }}>{categoryName}</SelectCategoryItem>
-                        ))}
-            </SelectCategoryList>
-                )}
+              onChange={handleChange}
+            />
+            <SelectIconSvg />
+            {isActive && (
+              <SelectCategoryList>
+                {baseCategories.map(({ _id, categoryName, type }) => (
+                  <SelectCategoryItem
+                    key={_id}
+                    name={categoryName}
+                    type={type}
+                    onClick={e => {
+                      setSelected(categoryName);
+                      setIsActive(false);
+                    }}
+                  >
+                    {categoryName}
+                  </SelectCategoryItem>
+                ))}
+              </SelectCategoryList>
+            )}
           </SelectWrapper>
-            {/* )} */}
+          {/* )} */}
           <AmoundDateWrapper>
             <AmoundWrapper>
-            <Amound name='amound' value={amound}  onChange={handleChange}/>     
-          </AmoundWrapper>
-          <DateWrapper>
-              <Calendar
-                name='date'
-                value={date}
-                onChange={handleChange}
-              />
-              <CalendarImg/>
+              <Amound name="amound" value={amound} onChange={handleChange} />
+            </AmoundWrapper>
+            <DateWrapper>
+              <Calendar name="date" value={date} onChange={handleChange} />
+              <CalendarImg />
             </DateWrapper>
-            </AmoundDateWrapper>
+          </AmoundDateWrapper>
           <CommentWrapper>
-          <Comments placeholder="Comment" name='comment' value={comment}  onChange={handleChange}/>
+            <Comments
+              placeholder="Comment"
+              name="comment"
+              value={comment}
+              onChange={handleChange}
+            />
           </CommentWrapper>
-                        <ModalButtonStyled type='submit'>ADD</ModalButtonStyled>
-                        <ModalButtonStyled onClick={handleClose}>CANCEL</ModalButtonStyled>
-            </ModalContainerStyled>
-        </ModalBackdrop>
+          <ModalButtonStyled type="submit">ADD</ModalButtonStyled>
+          <ModalButtonStyled onClick={handleClose}>CANCEL</ModalButtonStyled>
+        </ModalContainerStyled>
+      </ModalBackdrop>
     </>
   );
 }
